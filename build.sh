07c82d8f85d0d060985beb67adfb407e36945d3e
Renato Athaydes <renato@athaydes.com>
2022-06-04 17:12:44 +0200

    Fixing location of go files.

--- build.sh
+++ build.sh
@@ -2,13 +2,8 @@
 
 set -e
 
+FUNCTIONS=website/netlify/functions/analytics
+
 echo "Build go"
-go build .
-echo "Tests"
-go test .
-echo "Clean website/netlify dir"
-rm -rf website/netifly/
-echo "Copy go files to website/netlify dir"
-mkdir -p website/netlify/functions/analytics/
-cp *.go website/netlify/functions/analytics/
+cd $FUNCTIONS && go test .
 echo "Built successfully"
